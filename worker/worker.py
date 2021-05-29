9e0f3ac5ddf52c7b0c2db15ef1f498f933224a8d
dbampalikis <dimitris.bampalikis@gmail.com>
2021-05-29 14:23:28 +0200

    Added support for different frameworks

--- worker/worker.py
+++ worker/worker.py
@@ -11,7 +11,20 @@ import logging
 import sys
 import psycopg2
 
-
+p_languages = {
+    'python': '(testproject|gauge|jasmin|pytest|unittest|Nose2|Testify)',
+    'java': '(serenity|testproject|sahi|galen|gauge|OpenTest|karate-dsl|junit|spock|testng|dbUnit|greenmail|h2database|jmockit1|junit5|embedded-redis|mockito|restassured|android-screenshot-lib)',
+    'javascript': '(cypress|testproject|sahi|galen|gauge|WebDriverIO|karate-dsl|jasmin|mocha|jest)',
+    'c#': '(testproject|gauge|nunit)',
+    'go': 'gauge',
+    'ruby':  '(gauge|jasmin)',
+    'visualbasic': 'nunit',
+    'scala': '(citrus|restassured)',
+    'groovy': '(citrus|spock)',
+    'haskell': 'QuickCheck',
+    'rust': 'test',
+    'elixir': 'test',
+}
 
 default = False
 logging.basicConfig(level=logging.INFO)
@@ -32,49 +45,58 @@ def clone_repo(clone_url, language, full_name):
     logging.info("Starting cloning")
     tests = False
     ci_cd = False
-    #ci_cd_list = ['.circleci', 'bamboo-specs', 'workflows']
     with tempfile.TemporaryDirectory() as tmpdirname:
         Repo.clone_from(clone_url, tmpdirname)
         repo = Repo(tmpdirname)
         commits_count = repo.git.rev_list('--count', 'HEAD')
         logging.info('Commits count: %s' % commits_count)
-        tests, ci_cd = static_analysis(tmpdirname)
+        tests, ci_cd = static_analysis(tmpdirname, language)
         logging.info('Tests: %s' % tests)
         logging.info('CI/CD: %s' % ci_cd)
     database_insert(full_name, language, commits_count, tests, ci_cd)
 
-def static_analysis(tmpdirname):
+def static_analysis(tmpdirname, language):
     logging.info("Starting static analysis")
     tests = False
     ci_cd = False
-    ci_cd_list = ['.circleci', 'bamboo-specs', 'workflows']
-    
+    ci_cd_list = ['.circleci', 'bamboo-specs', 'workflows', '.jenkins']
+    test_reg = re.compile(".*(test|spec).*", re.IGNORECASE)
+    if language in p_languages:
+        langTestReg = re.compile(p_languages[language].lower(), re.IGNORECASE)
+
     for root, dirs, files in walk(tmpdirname):
         logging.info('Root: %s' % root)
         for dir in dirs:
-            if (dir in ci_cd_list ) or (dir == 'workflows'): # File the path to workflows and root == '.github/workflows'): 
+            if dir in ci_cd_list:
                 logging.info('Directory for ci/cd: %s' % dir)
                 ci_cd = True 
             if not tests:
                 for file_name in files:
-                    # Case opening each file
-                    # TODO: Add more types of test or do it per language
-                    with open(os.path.join(root, file_name),'r') as f:
-                        logging.debug('File name opened: %s' % file_name)
-                        try:
-                            data = f.read()
-                            data = data.lower()
-                            if 'pytest' in data:
-                                logging.info('File for testing: %s' % file_name)
-                                tests = True
-                                break
-                        except:
-                            continue
+                    if language in p_languages:
+                        logging.info("Language: %s" %language)
+                        # Case opening each file
+                        # TODO: Add more types of test or do it per language
+                        with open(os.path.join(root, file_name),'r') as f:
+                            logging.debug('File name opened: %s' % file_name)
+                            try:
+                                data = f.read()
+                                data = data.lower()
+
+                                if re.match(langTestReg, data):
+                                    logging.info('File for testing: %s' % file_name)
+                                    tests = True
+                                    break
+                            except:
+                                continue
                     # Case checking only the name of the file
-                    if re.match(".*test.*", file_name, re.IGNORECASE):
-                        logging.info('File name for testing: %s' % file_name)
-                        tests = True
-                        break
+                    else:
+                        if re.match(test_reg, file_name):
+                            logging.info('File name for testing: %s' % file_name)
+                            tests = True
+                            break
+                    if '.travis.yml' in file_name:
+                        ci_cd = True
+
             if ci_cd and tests:
                 return (True, True)
     # except Exception as e:
@@ -126,8 +148,8 @@ def database_insert(name, language, commits, tests, ci_cd):
 
 def main():
     client = pulsar.Client('pulsar://pulsar:6650')
-    consumer = client.subscribe('my-topic', subscription_name='worker')
-    logging.info("At least")
+    consumer = client.subscribe('repos', subscription_name='worker')
+
     while True:
         message = consumer.receive()
         try:
